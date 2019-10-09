79240e6a76973b49668e148c3989d0e1a26a67ae
Your Name <you@example.com>
2019-10-09 18:06:48 +0000

    part 2

--- MyOutlook1904/__init__.py
+++ MyOutlook1904/__init__.py
@@ -6,7 +6,7 @@ import json
 import datetime
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    name = req.params.get('code')
+    name = req.params.get('access_code')
     code = name
     #code = 'OAQABAAIAAACQN9QBRU3jT6bcBQLZNUj7qBV60oAQ9oGUYN5qdk1T4wvhAiXI_R40VOiyTVj4u62LeuFp_lH3rx340j5piourAzMT_JpqQu022sRIxDt5w1XP54aaLXk8dBLquhwRtxg9VgAUL0FHX9t5h06EDHTfh9VoCtVtWxsjymanRJwb1h_TvTtTejFFROnTDl3w03xMIu5twHem9txAyUnrDz6jRwTKLPjAYLJkDFmxbkSsPp77VemcM7sm5xTNaORj52_14i6RLPbKZlIcVtPYPVSR-hmHGvq3QgeIPsJUo5qLaZL4vfvjmSk_XBAkyxyPfuOUCp4N7ud-4bc7-y7oa-syJ4JUBOWBjqoXTggcdu4r0byx8fy-SsaH_FGoO7ydMJLb0yg-4ZfS6J-AYfskMXKdLLqj62w4DFRYW3kkWZWmgy5U3nYsoOqnMK46Bw4-WGv2yg361yONaZCYBJ7ocdl9_xFZHyiyyanyUbRol8WnEKv9UYuCHnSPapc0QfvncxmJyloVkXrJ_VekZYX3lH_Zk5RhFh4IBqqFRsTGWF6NWrQoYCSfqoPm49b3SLYh1b19pC36SnfwABb3HOl_WGc5ZQQHl8g0ZHyic-joopmfCGsgTQIKKQivy2y6WN9GjVNCggXHLvGtgYdoOMzAdNjJZOYEqXHJSV6PIHpFOMe9ALdvF0GbU3zIoGicxfjAQqycI-F-ZiVMtnB5KiTggaOlgZs5IBxswJ-FKa1fyARxA8oSW57wtCoDRJjIxT6hPBkgAA'
     x_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
@@ -17,14 +17,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     data_auth = {'grant_type':'authorization_code','client_id':'fa2924d5-8237-48f1-82ae-d9256243adef','code':code,'redirect_uri':'https://13.127.109.20/flask/token','client_secret':
 '/KFT/KgBc752JPLFx6_B=lYOfzOvzaB2',}
     #name = req.params.get('name')
-    email = req.params.get('email')
+    Email = req.params.get('email')
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get('codee')
 
     if name:
         r = requests.post(x_url,headers=header,data=data_auth)
@@ -32,7 +32,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         auth_token = data_auth['access_token']
         refresh_token = data_auth['id_token']
         #token = auth_token}
-        calender = getCal(auth_token)
+        calender = getCal(auth_token,Email)
         return func.HttpResponse(calender)
     else:
         return func.HttpResponse(
@@ -40,7 +40,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              status_code=400
         )
 
-def getCal(auth_token):
+def getCal(auth_token,Email):
     cal = 'https://graph.microsoft.com/v1.0/me/calendar/getSchedule'
     cal_header = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
@@ -50,7 +50,7 @@ def getCal(auth_token):
 
         }
     cal_data = {  
-    "schedules":[email],
+    "schedules":[Email],
     'startTime': {
         'dateTime': '2019-10-08T22:00:00',
         'timeZone': 'Pacific Standard Time',
